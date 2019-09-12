22ae5821746e90136cc062162a5f460e4118453f
martypowell <jpowel25@gmail.com>
2019-09-12 12:16:32 -0400

    commented on what program needs to do

---
+++ index.js
@@ -0,0 +1,11 @@
+// Read a Log File
+
+// Filter Logs by HTTP_ACTION = GET over standard port 80, should exclude requests beginning with "207.114"
+
+// Get IPS Addresses as a list
+
+// Group IPAddresses by Number of Requests Made per IP Address
+
+// Sort the Grouped IP Addresses, by Number of Requests (most first), then by IP octets of greater values listed first
+
+// Save the Grouped IP Addresses to a results.csv file, format = number of requests, ip address
