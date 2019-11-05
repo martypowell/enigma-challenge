0ff4413ec6a2c021a62ef3d9bd68b80e16eba81f
Marty Powell <jpowel25@gmail.com>
2019-11-05 16:02:48 -0500

    periodic check in of the sorting function

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import { GetIpAddressAsNumber } from "./utilities";
 const fs = require("fs");
 const args = require("yargs").argv;
 
@@ -77,9 +78,22 @@ getLogs(logFilePath, logs => {
     {}
   );
 
-  console.log("filtered", ipAddressByCount);
-});
+  // Sort the Grouped IP Addresses, by Number of Requests (most first), then by IP octets of greater values listed first
+  const sortedIpAddressesByCount = Object.entries(ipAddressByCount)
+    .map(([ipAddress, count]) => ({ ipAddress, count }))
+    .sort((a, b) => {
+      if (a.count > b.count) {
+        return -1;
+      } else if (a.count < b.count) {
+        return 1;
+      }
 
-// Sort the Grouped IP Addresses, by Number of Requests (most first), then by IP octets of greater values listed first
+      const aIpAddressAsNumber = GetIpAddressAsNumber(a.ipAddress);
+      const bIpAddressAsNumber = GetIpAddressAsNumber(b.ipAddress);
+      console.log("test", aIpAddressAsNumber);
+    }); // sort by number of requests with the most being first
+
+  console.log("filtered", sortedIpAddressesByCount);
+});
 
 // Save the Grouped IP Addresses to a results.csv file, format = number of requests, ip address
