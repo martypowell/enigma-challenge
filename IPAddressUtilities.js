ffc4da216ace9f8098d30b8d82fba01920ace467
Marty Powell <jpowel25@gmail.com>
2019-11-18 07:34:43 -0500

    rolled back changes

--- IPAddressUtilities.js
+++ IPAddressUtilities.js
@@ -3,7 +3,9 @@
  * @param {*} ipAddress
  */
 const GetIpAddressAsNumber = ipAddress => {
-  const [part1, part2, part3, part4] = ipAddress.split(".").map(parseInt);
+  const [part1, part2, part3, part4] = ipAddress
+    .split(".")
+    .map(part => parseInt(part));
   return (
     part1 * Math.pow(256, 3) + part2 * Math.pow(256, 2) + part3 * 256 + part4
   );
